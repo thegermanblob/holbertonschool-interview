77e226a54cdf2d5ac2fe59a1f33ca61680f028a2
Thegermanblob <thegermanblob@gmail.com>
2021-12-09 14:46:17 -0400

    Add line

---
+++ 0-lockboxes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """ checks if all boxes can be opened """
+    keys = [0]
+    for box in boxes:
+        if box not in keys:
+            keys = keys + box
+    return len(keys) == len(boxes)
